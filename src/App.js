89b1bd0c732373bfeff61854061446a007307ca8
Benjie <bgilross@gmail.com>
2024-07-19 17:37:13 -0700

    Robo Friends finishing touches

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import CardList from './CardList'
-import { robots } from './robots'
 import SearchBox from './SearchBox'
 import React from 'react'
+import Scroll from './Scroll'
+import ErrorBoundary from './ErrorBoundary'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }))
   }
 
   onSearchChange = (event) => {
@@ -27,7 +34,11 @@ class App extends React.Component {
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
+        </Scroll>
       </div>
     )
   }
